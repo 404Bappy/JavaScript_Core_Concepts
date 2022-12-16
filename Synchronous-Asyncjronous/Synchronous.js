018117e44be22c61ac5dcacfa2948e75ee1df0de
Bappy71 <19103076@iubat.edu>
2022-12-16 06:48:30 +0600

    first commit

---
+++ Synchronous-Asyncjronous/Synchronous.js
@@ -0,0 +1,12 @@
+const processOrders = (customer) => {
+    console.log('processing Order for customer 1');
+
+    var currentTime = new Date().getTime();
+    whileb(currentTime + 3000 >= new Date().getTime);
+
+    console.log('order process for customer 1');
+};
+
+console.log('take order for customer 1');
+processOrders();
+console.log('complete order for customer 1');
