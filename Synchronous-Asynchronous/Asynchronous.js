391c012c121010e61e775356c33d97d73664d004
Bappy71 <19103076@iubat.edu>
2022-12-17 04:51:27 +0600

    first commit

--- Synchronous-Asynchronous/Asynchronous.js
+++ Synchronous-Asynchronous/Asynchronous.js
@@ -6,6 +6,10 @@ console.log('im the second person here');
 //setTimeout(doSomthing , 5000);
 setTimeout(function() {
     console.log('Summer of 69');
-} , 6000)
+} , 6000);
+
+setTimeout( () => {
+    console.log('this is arrow function');
+},3000 )
 console.log('im the third prson here');
 console.log('im the fourth person heres');
