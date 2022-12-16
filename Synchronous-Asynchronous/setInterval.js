59a7833aea55325fcdb2e64805d4c3c0259bb213
Bappy71 <19103076@iubat.edu>
2022-12-17 05:05:47 +0600

    first commit

--- Synchronous-Asynchronous/setInterval.js
+++ Synchronous-Asynchronous/setInterval.js
@@ -4,3 +4,12 @@ setInterval( () => {
 },2000 )
 
 console.log('jashim');
+
+
+
+
+
+
+//////// SetInterval( ) is used for finding the our put in a certain amount of time.
+//////// SetTimeout( ) is used for nake js Asynchronous . it print the due work letter . by scheduling how much time it will take .
+//////// 
