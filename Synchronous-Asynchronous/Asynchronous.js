c30fa8ee2b1c7cda11dc2ed1ede05ec472210bc9
Bappy71 <19103076@iubat.edu>
2022-12-17 03:26:03 +0600

    first commit

--- Synchronous-Asynchronous/Asynchronous.js
+++ Synchronous-Asynchronous/Asynchronous.js
@@ -1,4 +1,8 @@
+function doSomthing() {
+    console.log('im coding js in vs code');
+}
 console.log('First: Ami Shobar age');
 console.log('im the second person here');
+setTimeout(doSomthing);
 console.log('im the third prson here');
 console.log('im the fourth person heres');
