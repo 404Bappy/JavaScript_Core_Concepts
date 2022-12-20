e5ace4cf3c6cb231243addd1db0c35ff8e39c62b
Bappy71 <19103076@iubat.edu>
2022-12-20 23:55:33 +0600

    first commit

--- Comparision-Operator/==.js
+++ Comparision-Operator/==.js
@@ -5,3 +5,5 @@ console.log(typeof record);
 if (record == '10') {
     console.log("That iS correct");
 }
+
+//Here We can see that == Comparision Operator will check only value from left and right sight. it will not check the data type.
