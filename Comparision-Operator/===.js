c00c1989e0a52b8d0d3f93391985a7fdf44fb780
Bappy71 <19103076@iubat.edu>
2022-12-25 13:59:43 +0600

    first commit

---
+++ Comparision-Operator/===.js
@@ -0,0 +1,9 @@
+record = 10;
+console.log(record);
+console.log(typeof record);
+
+if (record === '10') {
+    console.log("That iS correct");
+}
+
+//Here We can see that === Comparision Operator will check  value  left and right sight both. it will  check the data type also.
