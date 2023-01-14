6010c1f175c0b23c9fffef051a12d15a8a674e7f
Bappy71 <19103076@iubat.edu>
2023-01-15 01:28:44 +0600

    Initaial commit

---
+++ Destructuring-ES6/destructuring-object.js
@@ -0,0 +1,17 @@
+let obj = {
+    name : "Bappy",
+    age : 26
+};
+
+let {name, age} = obj;
+
+console.log(name , age);
+
+
+let object = {
+    name1 :"Bashar",
+    Age :25
+}
+
+let {name1, Age} = object;
+console.log(name1, Age);
