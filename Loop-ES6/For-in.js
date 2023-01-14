8da0c078cad26b0d279fe073524a36ecfad6e8b5
Bappy71 <19103076@iubat.edu>
2023-01-14 18:10:06 +0600

    Initaial commit

---
+++ Loop-ES6/For-in.js
@@ -0,0 +1,20 @@
+
+/// For of use for array looping ///
+let numbers = [1,2,3,4,5,6];
+
+for( let number of numbers){
+    console.log(number);
+}
+
+
+/// For In use for obj looping ///
+let obj = {
+    name: 'Bappy',
+    age: 20
+};
+for (let prop in obj){
+    console.log(obj[prop]);
+}
+
+
+
