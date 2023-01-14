764160594b747783947ba1bd7659e751c8fa2b07
Bappy71 <19103076@iubat.edu>
2023-01-15 01:05:48 +0600

    Initaial commit

---
+++ Destructuring-Array/destructuring.js
@@ -0,0 +1,30 @@
+// // Destructuring Old Procedure before ES6///
+
+let numbers = [1,2,3,4];
+let a = numbers[0];
+let b = numbers[1];
+let c = numbers[2];
+let d = numbers[3];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+//  Destructuring New Procedure in JS ES6 ////
+
+let Numbers = [1,2,3,4,5,6,7,8,9];
+
+let [a,b,c,d,e,f,g,h,i] = Numbers;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+
