160948ec5547b8e1e949edddd727f21a571a1b72
Bappy71 <19103076@iubat.edu>
2023-01-14 15:24:28 +0600

    Initaial commit

---
+++ Arrow-Functions/ArrowFunc.js
@@ -0,0 +1,15 @@
+// Old Fuctions =========>>>>
+function func(){
+    console.log("Hello There");
+}
+func();
+
+// ES6 New Arrow Function =========>>>
+let Funct = () =>{
+    console.log("This is Bappy");
+}
+Func();
+
+//For Single Line Arrow Function Will be Shorter======>>>
+let Func = () => console.log("From IUBAT ");
+Func()
