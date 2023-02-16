4fdf8494b7309066f39d06063fee4c68fdea7583
Bappy71 <19103076@iubat.edu>
2023-02-16 15:19:52 +0600

    first commit

--- Destructuring-ES6/destructuring-array.js
+++ Destructuring-ES6/destructuring-array.js
@@ -1,6 +1,6 @@
 // // Destructuring Old Procedure before ES6///
 
-let numbers = [1,2,3,4];
+let numbers = [1, 2, 3, 4];
 let a = numbers[0];
 let b = numbers[1];
 let c = numbers[2];
@@ -14,9 +14,9 @@ console.log(d);
 
 //  Destructuring New Procedure in JS ES6 ////
 
-let Numbers = [1,2,3,4,5,6,7,8,9];
+let Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let [a,b,c,d,e,f,g,h,i] = Numbers;
+let [a, b, c, d, e, f, g, h, i] = Numbers;
 
 console.log(a);
 console.log(b);
