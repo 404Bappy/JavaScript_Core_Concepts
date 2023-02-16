d212d8f0953baa0aed4fd6444eb430e04274d530
Bappy71 <19103076@iubat.edu>
2023-02-17 00:23:31 +0600

    first commit

--- Destructuring-ES6/destructuring-max.js
+++ Destructuring-ES6/destructuring-max.js
@@ -1,6 +1,13 @@
-//ARRAY//
+// //ARRAY//
+// const numbers = [1, 2, 3, 4, 5];
+
+// [num1, num2, num3] = numbers;
+
+// console.log(num1, num2, num3);
+
+
 const numbers = [1, 2, 3, 4, 5];
 
-[num1, num2, num3] = numbers;
+[num1, , num3] = numbers;
 
-console.log(num1, num2, num3);
+console.log(num1,  num3);
