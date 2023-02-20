87d804f6134e3b4fab53e0facf9c83f1c173c90d
Bappy71 <19103076@iubat.edu>
2023-02-20 14:24:21 +0600

    first commit

--- Spread-Operator/Spread.js
+++ Spread-Operator/Spread.js
@@ -1,11 +1,11 @@
-// var number = [1, 3, 4, 5, 6, 9, 7, 2];
-// console.log(Math.max(number)); //==> Here The Output Will NAN(Not A Number)
+var number = [1, 3, 4, 5, 6, 9, 7, 2];
+console.log(Math.max(number)); //==> Here The Output Will NAN(Not A Number)
 
-// // =========>>>>> BY Using ES6 We ca add Spread operator With it (...)
-// //==> Now 
+// =========>>>>> BY Using ES6 We ca add Spread operator With it (...)
+//==> Now 
 
-// var numbers = [1, 2, 4, 7, 3, 5, 6, 9, 0];
-// console.log(Math.max(...numbers));
+var numbers = [1, 2, 4, 7, 3, 5, 6, 9, 0];
+console.log(Math.max(...numbers));
 
 
 const numbers = [1, 2, 3, 4];
